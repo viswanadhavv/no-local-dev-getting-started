26caface132951859d9aca7d1b0f77628a99ffcc
viswanadhavv <viswanadha.v.v@gmail.com>
2018-03-21 00:05:38 +0530

    Update app.rb

--- app.rb
+++ app.rb
@@ -11,13 +11,13 @@ end
 
 
 class Contact < ActiveRecord::Base
-  self.table_name = 'salesforce.contact'
+  self.table_name = 'sfdcvickyherokudev01.contact'
 end
 
-#get "/contacts" do
-#  @contacts = Contact.all
-#  erb :index
-#end
+get "/contacts" do
+  @contacts = Contact.all
+  erb :index
+end
 
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
